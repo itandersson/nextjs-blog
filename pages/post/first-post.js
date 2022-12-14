dd0ea6fc1c0bc45d26a63a744196d1c5a53ea114
Joakim Andersson <joakim@ktv-sjobo.se>
2022-12-14 11:10:56 +0100

    Adding an images

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,4 +1,5 @@
-import Script from 'next/script';
+import Image from 'next/image';
+import Script from "next/script";
 import Head from "next/head";
 import Link from "next/link";
 
@@ -16,6 +17,12 @@ export default function FirstPost() {
         }
       />
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
